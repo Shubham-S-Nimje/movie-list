9053290fdc3c8605e68995c03f39bf8d71ffa9c9
Shubham Nimje <shubhamsnimje@gmail.com>
2025-03-13 13:34:25 +0530

    jio hotstart

---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-movie',
+  standalone: false,
+  templateUrl: './movie.component.html',
+  styleUrl: './movie.component.css',
+})
+export class MovieComponent {
+  @Input() name = '';
+  @Input() image = '';
+  @Input() videoUrl = '';
+}
